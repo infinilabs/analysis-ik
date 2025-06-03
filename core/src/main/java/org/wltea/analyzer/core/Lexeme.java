ec38cbd4ec346f6e6f2a71a5a7ce5501983bc7b8
richard <richard.lee@big-data.com.tw>
2025-06-03 17:03:40 +0800

    change Lexeme.compareTo sorting rules: shorter term first

--- core/src/main/java/org/wltea/analyzer/core/Lexeme.java
+++ core/src/main/java/org/wltea/analyzer/core/Lexeme.java
@@ -120,11 +120,12 @@ public class Lexeme implements Comparable<Lexeme>{
             return -1;
         }else if(this.begin == other.getBegin()){
         	//词元长度优先
-        	if(this.length > other.getLength()){
+			//長度短的優先
+        	if(this.length < other.getLength()){
         		return -1;
         	}else if(this.length == other.getLength()){
         		return 0;
-        	}else {//this.length < other.getLength()
+        	}else {//this.length > other.getLength()
         		return 1;
         	}
         	
